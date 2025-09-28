37f591e68ab9aca97b83d1743f59fac9c6be17fc
Danyl Brahin <inicerer141@gmail.com>
2025-09-28 15:30:45 +0300

    Increase version for UI kit

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -47,7 +47,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.dezdeqness.support",
         artifactId = "core-ui",
-        version = "0.3.1-beta01"
+        version = "0.3.1-beta02"
     )
 
     pom {
