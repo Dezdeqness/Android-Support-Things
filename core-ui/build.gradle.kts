5c75310195c1d21b2f9cedc16d4c7a7a5fc733ec
Dezdeqness <inicerer141@gmail.com>
2026-05-23 15:20:57 +0300

    Update core-ui version

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -47,7 +47,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.dezdeqness.support",
         artifactId = "core-ui",
-        version = "0.3.1-beta08"
+        version = "0.3.1"
     )
 
     pom {
