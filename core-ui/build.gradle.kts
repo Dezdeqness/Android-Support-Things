f00e782eb18de705a78c6cf22a7fbb8f3301eada
Dezdeqness <inicerer141@gmail.com>
2026-05-23 15:34:40 +0300

    Merge remote-tracking branch 'origin/develop' into develop

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -47,7 +47,7 @@ mavenPublishing {
     coordinates(
         groupId = "com.dezdeqness.support",
         artifactId = "core-ui",
-        version = "0.3.1"
+        version = "0.3.2"
     )
 
     pom {
